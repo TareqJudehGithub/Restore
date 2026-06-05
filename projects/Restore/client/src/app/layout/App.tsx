f84e365f62f3bf2e7c9427ae0e491ad0b9745b39
TareqJudehGithub <tareq.joudeh@gmail.com>
2026-06-05 18:37:38 +0300

    Implemented Toggle Dark Mode On/Off for the UI, by implementing a dedicated state where the user clicks on dark/light icons to switch between the two modes.

--- projects/Restore/client/src/app/layout/App.tsx
+++ projects/Restore/client/src/app/layout/App.tsx
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 
 import type { Product } from "../models/product";
 import Catalog from "../features/catalog/Catalog";
-import { Container } from "@mui/material";
+import {
+	Container,
+	createTheme,
+	ThemeProvider,
+	Box,
+	CssBaseline,
+} from "@mui/material";
 import NavBar from "./NavBar";
 
 function App() {
 	// States
 	const [products, setProducts] = useState<Product[]>([]);
-
+	const [darkMode, setDarkMode] = useState<boolean>(true);
 	useEffect(() => {
 		const url: string = "https://localhost:5001/api/products";
 
@@ -21,15 +27,43 @@ function App() {
 		fetchData();
 	}, []);
 
-	// Methods
+	// Handlers
+	function handleDarkMode(): void {
+		return setDarkMode((darkMode) => !darkMode);
+	}
+
+	// Dark Mode theme
+	//const darkMode: boolean = true;
+	const paletteType = darkMode ? "dark" : "light";
+
+	const theme = createTheme({
+		palette: {
+			mode: paletteType,
+			background: {
+				// Set background color based on palette mode
+				default: paletteType === "light" ? "#eaeaea" : "#121212",
+			},
+		},
+	});
 
 	return (
-		<>
-			<NavBar />
-			<Container maxWidth="xl" sx={{ mt: 14 }}>
-				<Catalog products={products} />
-			</Container>
-		</>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<NavBar darkMode={darkMode} OnDarkMode={handleDarkMode} />
+			<Box
+				sx={{
+					minHeight: "100vh",
+					background: darkMode
+						? "radial-gradient(circle, #172f9b, #111b27)"
+						: "radial-gradient(circle, #baecf9, #f0f9ff)",
+					py: 6,
+				}}
+			>
+				<Container maxWidth="xl" sx={{ mt: 8 }}>
+					<Catalog products={products} />
+				</Container>
+			</Box>
+		</ThemeProvider>
 	);
 }
 
--- projects/Restore/client/src/app/layout/NavBar.tsx
+++ projects/Restore/client/src/app/layout/NavBar.tsx
@@ -1,11 +1,25 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { LightMode, DarkMode } from "@mui/icons-material";
 
-export default function NavBar() {
+export default function NavBar({ darkMode, OnDarkMode }: NavBarProps) {
 	return (
 		<AppBar position="fixed">
 			<Toolbar>
 				<Typography variant="h6">Re-Store</Typography>
+				<IconButton onClick={OnDarkMode}>
+					{/* Dark mode toggle on/off */}
+					{darkMode ? (
+						<DarkMode sx={{ color: "silver" }} />
+					) : (
+						<LightMode sx={{ color: "yellow" }} />
+					)}
+				</IconButton>
 			</Toolbar>
 		</AppBar>
 	);
 }
+
+type NavBarProps = {
+	darkMode: boolean;
+	OnDarkMode: () => void;
+};

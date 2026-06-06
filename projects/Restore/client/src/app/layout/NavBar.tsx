f976d300e5a271932a649eb50ad0301b903e58bb
TareqJudehGithub <tareq.joudeh@gmail.com>
2026-06-06 17:01:24 +0300

    Styled Navbar links CSS properties conditionally and according to the current active theme (dark or light).

--- projects/Restore/client/src/app/layout/NavBar.tsx
+++ projects/Restore/client/src/app/layout/NavBar.tsx
@@ -18,17 +18,37 @@ export default function NavBar({ darkMode, OnDarkMode }: NavBarProps) {
 				sx={{
 					display: "flex",
 					justifyContent: "space-between",
+					typography: "h6",
 				}}
 			>
 				<Box
 					sx={{
 						display: "flex",
 						alignItems: "center",
+						textDecoration: "none",
+						fontSize: 24,
+						textTransform: "uppercase",
 					}}
 				>
-					<Typography component={NavLink} to={"/"} variant="h6">
-						Re-Store
-					</Typography>
+					{darkMode ? (
+						<Typography
+							component={NavLink}
+							to={"/"}
+							variant="inherit"
+							sx={navLinkStyleDark}
+						>
+							Re-Store
+						</Typography>
+					) : (
+						<Typography
+							component={NavLink}
+							to={"/"}
+							variant="inherit"
+							sx={navLinkStyleLight}
+						>
+							Re-Store
+						</Typography>
+					)}
 
 					<IconButton onClick={OnDarkMode}>
 						{/* Dark mode toggle on/off */}
@@ -44,16 +64,27 @@ export default function NavBar({ darkMode, OnDarkMode }: NavBarProps) {
 						display: "flex",
 					}}
 				>
-					{midLinks.map(({ title, path }) => (
-						<ListItem
-							component={NavLink}
-							to={path}
-							key={path}
-							sx={{ color: "inherit", typography: "h6" }}
-						>
-							{title.toUpperCase()}
-						</ListItem>
-					))}
+					{midLinks.map(({ title, path }) =>
+						darkMode ? (
+							<ListItem
+								component={NavLink}
+								to={path}
+								key={path}
+								sx={navLinkStyleDark}
+							>
+								{title.toUpperCase()}
+							</ListItem>
+						) : (
+							<ListItem
+								component={NavLink}
+								to={path}
+								key={path}
+								sx={navLinkStyleLight}
+							>
+								{title.toUpperCase()}
+							</ListItem>
+						),
+					)}
 				</List>
 				<List
 					sx={{
@@ -67,16 +98,27 @@ export default function NavBar({ darkMode, OnDarkMode }: NavBarProps) {
 							</Badge>
 						</IconButton>
 					</Box>
-					{rightLinks.map(({ title, path }) => (
-						<ListItem
-							component={NavLink}
-							to={path}
-							key={path}
-							sx={{ color: "inherit", typography: "h6" }}
-						>
-							{title.toUpperCase()}
-						</ListItem>
-					))}
+					{rightLinks.map(({ title, path }) =>
+						darkMode ? (
+							<ListItem
+								component={NavLink}
+								to={path}
+								key={path}
+								sx={navLinkStyleDark}
+							>
+								{title.toUpperCase()}
+							</ListItem>
+						) : (
+							<ListItem
+								component={NavLink}
+								to={path}
+								key={path}
+								sx={navLinkStyleLight}
+							>
+								{title.toUpperCase()}
+							</ListItem>
+						),
+					)}
 				</List>
 			</Toolbar>
 		</AppBar>
@@ -93,6 +135,29 @@ const rightLinks = [
 	{ title: "login", path: "/login" },
 	{ title: "register", path: "/register" },
 ];
+
+const navLinkStyleDark = {
+	color: "inherit",
+	textDecoration: "none",
+	":hover": {
+		color: "gray",
+		textShadow: "1px 1px grey",
+	},
+	"&.active": {
+		color: "#baecf9",
+	},
+};
+const navLinkStyleLight = {
+	color: "inherit",
+	textDecoration: "none",
+	":hover": {
+		color: "#baecf9",
+		textShadow: "1px 1px grey",
+	},
+	"&.active": {
+		color: "#baecf9",
+	},
+};
 
 type NavBarProps = {
 	darkMode: boolean;
